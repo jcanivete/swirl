da2dfc6a7b47985296ff34996512b624275b8600
Roberto <joseroberto.canivetecuissa@gmail.com>
2022-04-11 14:52:03 +0200

    New file for plot routines

--- swirl/__init__.py
+++ swirl/__init__.py
@@ -6,4 +6,5 @@ from .rortex import compute_rortex
 from .evcmap import radius, direction
 from .utilities import vector2D, create_U
 from .cluster import compute_delta, compute_rho, prepare_data, compute_dc, clustering
-from .vortex import detection
+from .vortex import detection 
+from .plots import plot_decision
